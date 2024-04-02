dfb1d1a77929dbfa6a050ba1f1cdc1baf0aa0194
duke-doki <145125297+duke-doki@users.noreply.github.com>
2024-04-02 18:40:29 +0300

    move models from tg bot

--- viberbotapp/models.py
+++ viberbotapp/models.py
@@ -51,3 +51,81 @@ class Address(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Bill(models.Model):
+    persons = models.ManyToManyField(
+        Person,
+        verbose_name='клиенты',
+        related_name='bills'
+    )
+    value = models.IntegerField(
+        verbose_name='номер лицевого счета'
+    )
+
+
+class Device(models.Model):
+    address = models.TextField(
+        verbose_name='адрес счета'
+    )
+    device_title = models.TextField(
+        verbose_name='Прибор учета'
+    )
+    modification = models.TextField(
+        verbose_name='Модификация прибора учета'
+    )
+    serial_number = models.TextField(
+        verbose_name='Серийный номер прибора учета'
+    )
+    id_device = models.IntegerField(
+        verbose_name='ID счетчика'
+    )
+    bill = models.ForeignKey(
+        Bill,
+        verbose_name='Лицевой счет',
+        on_delete=models.CASCADE,
+        related_name='devices'
+    )
+
+
+class Rate(models.Model):
+    title = models.CharField(
+        'Обозначение',
+        max_length=20
+    )
+    id_tariff = models.IntegerField(
+        verbose_name='ID тарифа'
+    )
+    id_indication = models.IntegerField(
+        verbose_name='ID показания'
+    )
+    registration_date = models.DateTimeField(
+        verbose_name='Дата приёма'
+    )
+    readings = models.IntegerField(
+        verbose_name='Показания счетчика'
+    )
+    device = models.ForeignKey(
+        Device,
+        verbose_name='Устройство',
+        on_delete=models.CASCADE,
+        related_name='rates'
+    )
+    cost = models.FloatField(
+        verbose_name='Стоимость'
+    )
+
+
+class Favorite(models.Model):
+    person = models.ForeignKey(
+        Person,
+        verbose_name='юзер избранного ЛС',
+        on_delete=models.CASCADE,
+        related_name='favorites'
+    )
+    bill = models.ForeignKey(
+        Bill,
+        verbose_name='Избранный ЛС',
+        on_delete=models.CASCADE,
+        related_name='favorites'
+    )
